603d1760c9a7eb0b10405a09883b3aecda1f8ebf
TrumanGao <59617789@qq.com>
2025-03-18 11:37:45 +0800

    feat: enhance useSize hook with container and rem options, update HomePage to utilize dynamic sizing

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,21 @@
+import { useSize } from '@/hooks/useSize';
+import { useAppStore } from '@/states';
 import styles from './HomePage.module.less';
 
 export function Component() {
+  const { containerSize } = useAppStore();
+  useSize({
+    useContainer: true,
+  });
+
   return (
-    <div className={styles['home-container']}>
+    <div
+      className={styles['home-container']}
+      style={{
+        width: containerSize.width,
+        height: containerSize.height,
+      }}
+    >
       <div className={styles['home-header']}></div>
       <div className={styles['home-main']}>
         <div className={styles['home-content']}>{'HomePage'}</div>
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,10 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import { useSize } from '@/hooks/useSize';
 import styles from './Layout.module.less';
 
 export function Component() {
-  useSize();
-
   return (
     <div className={styles['layout-container']}>
       <div className={styles['layout-main']}>
