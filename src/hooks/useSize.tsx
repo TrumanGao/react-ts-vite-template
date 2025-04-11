6e6e7c5b310c4465b2eea46d610ea9da6b053049
TrumanGao <59617789@qq.com>
2025-04-11 11:04:54 +0800

    feat: enhance useSize hook with resize and orientation change callbacks, and refactor text size variables in styles

--- src/hooks/useSize.tsx
+++ src/hooks/useSize.tsx
@@ -6,40 +6,38 @@ const eventManager = new EventManager();
 
 /**
  * @param options
+ * @param options.resizeCallback resize事件的回调函数
+ * @param options.orientationChangeCallback orientationchange事件的回调函数
  * @param options.throttleTime 监听屏幕变化节流时间，默认500ms
  * @param options.useContainer 是否使用容器，默认false
  * @param options.containerOptions 容器配置
  * @param options.containerOptions.aspectRatio 容器宽高比，默认1.33
  * @param options.containerOptions.minPadding 容器距离屏幕边缘的最小间距，默认12
- * @param options.useRem 是否使用rem，默认false
- * @param options.remOptions rem配置
- * @param options.remOptions.minSize rem最小值
- * @param options.remOptions.maxSize rem最大值
  */
 export function useSize(
   options: {
+    resizeCallback?: (windowSize: {
+      innerWidth: number;
+      innerHeight: number;
+    }) => void;
+    orientationChangeCallback?: (orientation: ScreenOrientation) => void;
     throttleTime?: number;
     useContainer?: boolean;
     containerOptions?: {
       aspectRatio: number;
       minPadding: number;
     };
-    useRem?: boolean;
-    remOptions?: {
-      minSize?: number;
-      maxSize?: number;
-    };
   } = {},
 ) {
   const {
+    resizeCallback,
+    orientationChangeCallback,
     throttleTime = 500,
     useContainer = false,
     containerOptions = {
       aspectRatio: 1.33,
       minPadding: 36,
     },
-    useRem = false,
-    remOptions,
   } = options;
 
   const {
@@ -68,6 +66,10 @@ export function useSize(
         '--window-height',
         `${window.innerHeight}px`,
       );
+      resizeCallback?.call(null, {
+        innerWidth: window.innerWidth,
+        innerHeight: window.innerHeight,
+      });
     }, throttleTime);
     eventManager.addEventListener(
       'resize',
@@ -78,19 +80,11 @@ export function useSize(
 
     // 2. 处理屏幕方向变化
     eventManager.addEventListener(
-      'change',
-      () => {
-        console.log('useSize - change: ', screen.orientation);
-        updateScreenOrientation(screen.orientation);
-      },
-      'window_change_handleChange',
-      window,
-    );
-    eventManager.addEventListener(
       'orientationchange',
       () => {
         console.log('useSize - orientationchange: ', screen.orientation);
         updateScreenOrientation(screen.orientation);
+        orientationChangeCallback?.call(null, screen.orientation);
       },
       'window_orientationchange_handleOrientationchange',
       window,
@@ -100,11 +94,6 @@ export function useSize(
       eventManager.removeEventListener(
         'resize',
         'window_resize_handleResize',
-        window,
-      );
-      eventManager.removeEventListener(
-        'change',
-        'window_change_handleChange',
         window,
       );
       eventManager.removeEventListener(
@@ -156,34 +145,6 @@ export function useSize(
     }
   }
 
-  /**
-   * 设置根节点字体大小
-   */
-  function handleRootFontSize(
-    options: {
-      windowSize?: { innerWidth: number; innerHeight: number };
-    } = {},
-  ) {
-    const {
-      windowSize = {
-        innerWidth: window.innerWidth,
-        innerHeight: window.innerHeight,
-      },
-    } = options;
-
-    let rootFontSize =
-      Math.min(windowSize.innerWidth, windowSize.innerHeight) / 10;
-
-    if (remOptions?.minSize) {
-      rootFontSize = Math.max(rootFontSize, remOptions.minSize);
-    }
-    if (remOptions?.maxSize) {
-      rootFontSize = Math.min(rootFontSize, remOptions.maxSize);
-    }
-
-    document.documentElement.style.fontSize = `${rootFontSize}px`;
-  }
-
   useEffect(() => {
     const deviceType = getDeviceType();
     console.log('useSize - deviceType: ', deviceType);
@@ -196,20 +157,14 @@ export function useSize(
       });
     }
 
-    if (useRem) {
-      handleRootFontSize({
-        windowSize,
-      });
-    }
-
     if (deviceType === 'mobile') {
-      document.documentElement.style.setProperty('--text-size_18', '16px');
-      document.documentElement.style.setProperty('--text-size_16', '14px');
-      document.documentElement.style.setProperty('--text-size_14', '12px');
+      document.documentElement.style.setProperty('--text-size', '14px');
+      document.documentElement.style.setProperty('--text-size_small', '12px');
+      document.documentElement.style.setProperty('--text-size_large', '16px');
     } else {
-      document.documentElement.style.setProperty('--text-size_18', '18px');
-      document.documentElement.style.setProperty('--text-size_16', '16px');
-      document.documentElement.style.setProperty('--text-size_14', '14px');
+      document.documentElement.style.setProperty('--text-size', '16px');
+      document.documentElement.style.setProperty('--text-size_small', '14px');
+      document.documentElement.style.setProperty('--text-size_large', '18px');
     }
   }, [windowSize, screenOrientation]);
 }
